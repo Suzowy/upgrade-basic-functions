2d8b4fb05a83751be5a533a1b660c82c9602a58f
Suzowy <suzowy@gmail.com>
2023-03-12 21:57:42 +0100

    iteration#4

--- funciones.js
+++ funciones.js
@@ -31,7 +31,7 @@ function findLongestWord(param) {
 //Implemente la función denominada sumNumbers que toma un array de números como argumento y devuelve la suma de todos los números de la matriz. 
 //Puedes usar este array para probar tu función:
 
-const numbers = [1, 2, 3, 5, 45, 37, 58];
+//const numbers = [1, 2, 3, 5, 45, 37, 58];
 
 function sumNumbers(array) {
     let sum = 0;
@@ -41,4 +41,16 @@ function sumNumbers(array) {
     return console.log(sum);
   }
   
-  sumNumbers(numbers);
+  //sumNumbers(numbers);
+
+//Iteración #4: Calcular el promedio
+//Calcular un promedio es una tarea extremadamente común. Puedes usar este array para probar tu función:
+const numbers = [12, 21, 38, 5, 45, 37, 6];
+function average(param) {
+    let sum = 0;
+    for (let i = 0; i < param.length; i++) {
+      sum += param[i];
+    }
+    return console.log(sum / param.length);
+}
+average(numbers);
