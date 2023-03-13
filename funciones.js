b6fcf2d093e0e8b3149014c68e383898bc9128f3
Suzowy <suzowy@gmail.com>
2023-03-13 11:54:55 +0100

    iteration#8

--- funciones.js
+++ funciones.js
@@ -107,3 +107,23 @@ function averageWord(array) {
     finderName(nameFinder, "Xabier");
     finderName(nameFinder, "Paco");
     
+    //Iteration #8: Contador de repeticiones
+    //Crea una función que nos devuelva el número de veces que se repite cada una de las palabras que lo conforma.  Puedes usar este array para probar tu función:
+
+    const counterWords = [
+   'code', 'repeat','eat','sleep','code','enjoy','sleep', 'code','enjoy', 'upgrade', 'code'
+    ];
+    function repeatCounter(param) {
+      let count = {};
+      for (let i = 0; i < param.length; i++) {
+        if (param[i] in count) {
+          count[param[i]]++;
+        } else {
+          count[param[i]] = 1;
+        }
+      }
+      return console.log(count);
+    }
+  
+    repeatCounter(counterWords);
+    
