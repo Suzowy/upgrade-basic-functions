edf3aef37349c5f6c2e6531fc34ef23104628333
Suzowy <suzowy@gmail.com>
2023-03-13 11:48:49 +0100

    iteration#7

--- funciones.js
+++ funciones.js
@@ -1,5 +1,5 @@
 "use strict";
-const prompt = require("prompt-sync")();
+
 
 //**Iteración #1: Buscar el máximo**
 
@@ -91,3 +91,19 @@ function averageWord(array) {
     }
     
     removeDuplicates(duplicates);
+
+    //Iteración #7: Buscador de nombres
+    //Crea una función que reciba por parámetro un array y el valor que desea comprobar que existe dentro de dicho array - comprueba si existe el elemento, en caso que existan nos devuelve un true y la posición de dicho elemento y por la contra un false. Puedes usar este array para probar tu función:
+    const nameFinder = ['Peter','Steve','Tony','Natasha','Clint', 'Logan', 'Xabier', 'Bruce','Peggy','Jessica','Marc'
+    ];
+    function finderName(param, valor) {
+    
+     for (let i= 0; i< param.length; i++)
+     if (param[i] === valor) {
+      return console.log(true, i)
+     }
+     return console.log(false);
+    }
+    finderName(nameFinder, "Xabier");
+    finderName(nameFinder, "Paco");
+    
